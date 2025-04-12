8c9206fa1f3b787a5a5d53244e6b9c50fb00fc4c
Hamzah Asadullah <x3tut3@gmail.com>
2025-04-12 16:27:42 +0200

    Fixed to update with main.js

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ async function sleep(ms) { await new Promise(r => setTimeout(r, ms)); }
 function maxchars(string, chars) { return (string.length < chars) ? string : (string.substring(0, chars - 2) + ".."); }
 
 async function isAuthorized(pb) { try { await pb.collection("accounts").authRefresh(); return true; } catch { return false; }}
-async function updateData(newdata, pb) { try { await pb.collection("accounts").update(pb.authStore.record.id, { data: newdata }); return true; } catch (e) { console.error("Failed updating userdata:", e); return false; } }
+async function updateData(newdata, ref) { try { await ref.pb.collection("accounts").update(ref.pb.authStore.record.id, { data: newdata }); return true; } catch (e) { console.error("Failed updating userdata:", e); return false; } }
