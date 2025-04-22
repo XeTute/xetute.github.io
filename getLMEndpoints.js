1ad5bb0873d17b93cee6f8ea15cd2b9e5d28d1bb
Hamzah Asadullah <x3tut3@gmail.com>
2025-04-22 18:44:09 +0200

    Fixed TTS errors and a lot bugs, added loading screen

---
+++ getLMEndpoints.js
@@ -0,0 +1,100 @@
+function getLMEndpoints()
+{
+    const pollendpoint = `https://text.pollinations.ai/openai?seed=${Date.now() % 8192}`;
+    const maxgen = 8 * 1024;
+    const endpoints =
+    [
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "openai-large",
+            vision: true,
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "openai",
+            vision: true,
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "openai-reasoning",
+            vision: true,
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "llama", // 3.3 70B
+            vision: false,
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "llamascout",
+            vision: false,
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "mistral", // small 3
+            vision: true,
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "unity", // mistral uncensored
+            vision: true,
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "searchgpt",
+            vision: true,
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "phi", // 4 instruct
+            vision: true, // also supports audio
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "llama-vision", // 3.2 11B
+            vision: true,
+            generation: maxgen
+        },
+        {
+            url: pollendpoint,
+            key: "0",
+            model: "gemini", // 2.5 flash
+            vision: true, // also supports audio (for output too)
+            generation: maxgen
+        },
+        {
+            url: "https://ai.xetute.com/v1/chat/completions",
+            key: "0",
+            model: "0",
+            vision: true,
+            generation: maxgen / 2 // Not cuz of compute, local endpoint is only ~40tps, which is too slow to handle mult. req.
+        },
+        {
+            url: "https://api.groq.com/openai/v1/chat/completions",
+            key: "g" + "sk" + "_aYtFXeVhm5AtbOYaW4NSWGdyb3FYGzfOtgY3Q2hQsW6ICQetBOZE",
+            model: "gemma2-9b-it",
+            vision: false,
+            generation: maxgen
+        }
+    ];
+    return endpoints;
+}
